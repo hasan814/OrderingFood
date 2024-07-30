1448afa84c21b748053c85717556286dd8e0e9d6
hasan814 <h.mousavi910@gmail.com>
2024-07-30 15:28:06 +0330

    Finished Card Section

--- src/components/modules/Card.tsx
+++ src/components/modules/Card.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 const Card = () => {
   return (
-    <div className="bg-gray-200 p-4 rounded-lg text-center">
-      <img src="/pizza.png" alt="pizza" />
+    <div className="bg-gray-200 p-4 rounded-lg text-center group hover:bg-white hover:shadow-md hover:shadow-black/25 transition-all">
+      <div className="text-center">
+        <Image
+          src="/pizza.png"
+          alt="pizza"
+          width={96}
+          height={96}
+          className="block mx-auto"
+        />
+      </div>
       <h4 className="font-semibold text-xl my-3">Pepperoni Pizza</h4>
       <p className="text-gray-500 text-sm">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi nobis
--- src/components/modules/Menu.tsx
+++ src/components/modules/Menu.tsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 const Menu = () => {
   return (
     <section className="">
-      <div className="absolute h-full left-0 right-0 w-full justify-start">
+      <div className="absolute left-0 right-0 w-full justify-start">
         <div className="absolute left-0 -top-[70px] text-left -z-10">
           <Image src={"/sallad1.png"} width={109} height={189} alt="sallad" />
         </div>
@@ -12,13 +12,18 @@ const Menu = () => {
           <Image src={"/sallad2.png"} alt="sallad2" width={107} height={195} />
         </div>
       </div>
-      <div className="text-center">
+      <div className="text-center mb-4">
         <h3 className="uppercase text-gray-500 font-semibold leading-4">
           Check Out
         </h3>
         <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
       </div>
       <div className="grid grid-cols-3 gap-4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Card />
       </div>
     </section>
